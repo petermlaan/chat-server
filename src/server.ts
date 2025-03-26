345ca6bb84dd1a0cd088f0a32a563153724ed361
Peter Laan <plaan70@gmail.com>
2025-03-26 07:52:54 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -7,45 +7,29 @@ interface IConfig {
     rndspeed: number
 }
 
-console.log("starting websocket server...")
+console.log("Starting websocket server...")
 
-const cfg = fs.readFileSync("config.json")
-const cfgo = JSON.parse(cfg.toString()) as IConfig
-console.log("rndspeed: " + cfgo.rndspeed)
+const buffer = fs.readFileSync("config.json")
+const cfg = JSON.parse(buffer.toString()) as IConfig
+console.log("rndspeed: " + cfg.rndspeed)
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server, {cors: {
-    origin: "http://localhost:3000",
-}})
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    }
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
-
-server.listen(8080, () => {
-    console.log('listening on *:8080');
-});
-
-io.on("message", (args) => {
-    console.log("received message", args)
-    io.send("hello from the server")
+    socket.on('disconnect', () => console.log('user disconnected'))
+    socket.on("message", (msg) => {
+        console.log("received message: ", msg)
+        io.send("Broadcast: " + msg)
+    })
 })
 
-/*const wss = new WebSocket.Server({ port: 8080 });
-
-wss.on('connection', (ws: WebSocket) => {
-    console.log('New client connected');
-
-    ws.on('message', (message: string) => {
-        console.log(`Received message: ${message}`);
-        ws.send(`Server received your message: ${message}`);
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-});*/
+server.listen(8080, () => {
+    console.log('Listening on *:8080')
+})
