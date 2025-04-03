1437d8047472e74892677071580acd3e0965a4f3
Peter Laan <plaan70@gmail.com>
2025-04-03 20:39:55 +0200

    messages saved and sent to new users

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,5 @@
 export interface Msg {
+    chatroom_id: number;
     type: number; // 0 - message, 1 - user joined, 2  - user left
     user: string;
     msg: string;
@@ -6,4 +7,15 @@ export interface Msg {
 
 export interface Config {
     rndspeed: number
+}
+
+export interface ChatRoom {
+    id: number;
+    name: string;
+    messages?: Msg[];
+}
+
+export interface SocketData {
+    user: string;
+    roomId: number;
 }
