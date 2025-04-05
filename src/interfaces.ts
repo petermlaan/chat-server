e53f366c33a2d145628a4a11d498335d859f26f4
Peter Laan <plaan70@gmail.com>
2025-04-05 18:41:54 +0200

    update

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -14,5 +14,4 @@ export interface ChatRoom {
 
 export interface SocketData {
     user: string;
-    roomId: number;
 }
--- src/server.ts
+++ src/server.ts
@@ -11,69 +11,55 @@ program()
 async function program() {
     console.log("Starting websocket server...")
 
-    const app = express()
-    const servers: http.Server[] = []
-    const ios: Server[] = []
     const rooms = await dbGetChatRooms()
     const promisearr = rooms.map(r => dbGetMessages(r.id, msgBuffSize))
     const messagesarr = await Promise.all(promisearr)
     rooms.forEach((r, i) => r.messages = messagesarr[i])
-    rooms.forEach(() => servers.push(http.createServer(app)))
-    rooms.forEach((_, i) => ios.push(new Server(servers[i], { cors: { origin: "*" } })))
 
-    ios.forEach((io, i) => io.use((socket, next) => {
-        const socketData: SocketData = { 
-            user: socket.handshake.auth.token,
-            roomId: i,
-        }
+    const app = express()
+    const server = http.createServer(app)
+    const io = new Server(server, { cors: { origin: "*" } })
+    io.use((socket, next) => {
+        console.log("io.use")
+        const socketData: SocketData = { user: socket.handshake.auth.token }
         socket.data = socketData;
         next();
-    }))
-
-    ios.forEach(io => io.on('connection', (socket) => {
+    })
+    io.on('connection', socket => {
         console.log('a user connected')
-        const packet: Msg = {
-            chatroom_id: socket.data.roomId,
-            type: 1,
-            user: socket.data.user,
-            msg: "",
-            save: false,
-        }
-        io.send([packet])
-        socket.send(rooms[socket.data.roomId].messages)
-
         socket.on('disconnect', () => {
             console.log('user disconnected')
-            const packet: Msg = {
-                chatroom_id: socket.data.roomId,
-                type: 2,
-                user: socket.data.user,
-                msg: "",
-                save: false,
-            }
-            io.send([packet])
         })
-
-        socket.on("message", (msg) => {
-            const packet: Msg = {
-                chatroom_id: socket.data.roomId,
-                type: 0,
-                user: socket.data.user,
-                msg: msg as string,
-                save: true,
-            }
+        socket.on("message", (data) => {
+            console.log("message: ", data)
+            const msgin = data as Msg
+            msgin.save = true
+            io.to("" + msgin.chatroom_id).emit("message", [msgin])
             const messages = rooms[socket.data.roomId].messages
-            messages.push(packet)
+            messages.push(msgin)
             if (messages.length > 2 * msgBuffSize) {
                 const pruned = messages.splice(0, messages!.length - msgBuffSize)
                 console.log(pruned.length + " messages pruned")
                 dbInsertMessages(pruned)
             }
-            io.send([packet])
         })
-    }))
+        socket.on("join", (data) => {
+            console.log("join: ", data)
+            const msg = data as Msg
+            socket.join("" + msg.chatroom_id)
+            socket.send(rooms[socket.data.roomId].messages)
+            const packet: Msg = {
+                chatroom_id: msg.chatroom_id,
+                type: 3,
+                user: socket.data.user,
+                msg: "",
+                save: true,
+            }
+            io.to("" + msg.chatroom_id).emit("joined", packet)
+        })
+    })
 
-    servers.forEach((server, i) => server.listen(8080 + i, () => {
-        console.log("Listening on *:" + (8080 + i))
-    }))
+    server.listen(8080, () => {
+        console.log("Listening on *:" + 8080)
+    })
 }
